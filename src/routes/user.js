d00f4eadad173572048837730f7edc1fde08c2a7
aharon <aharonhezi1@gmail.com>
2021-01-27 13:34:15 +0200

    adding email and username vlidation requests

--- src/routes/user.js
+++ src/routes/user.js
@@ -12,7 +12,7 @@ router.post('/signup', async (req, res) => {
     try {
         console.log(req.body);
         const hashPass = bcrypt.hashSync(req.body.password, 8);
-        const user = await User.create({...req.body,password:hashPass})
+        const user = await User.create({ ...req.body, password: hashPass })
         res.send(user)
     } catch (e) {
         res.status(400).send(e)
@@ -25,12 +25,12 @@ router.post('/login', async (req, res) => {
         const { userName, password } = req.body
         const user = await User.findOne({ where: { userName } })
         // console.log(user.password);
-        const isValidPass= bcrypt.compareSync(password,user.password)
-        if (user&&!!isValidPass) {
-            const token = jwt.sign({userName}, process.env.JWT, {expiresIn:'24h'})
+        const isValidPass = bcrypt.compareSync(password, user.password)
+        if (user && !!isValidPass) {
+            const token = jwt.sign({ userName }, process.env.JWT, { expiresIn: '24h' })
             console.log(token);
             await Token.create({ userName, token })
-            res.send({ token,userName })
+            res.send({ token, userName })
         } else {
             throw new Error({ error: 'wrong user name or password' })
         }
@@ -44,8 +44,8 @@ router.get('/authUser', async (req, res) => {
     try {
         const token = req.header('token')// .replace('Bearer ', '')
         const user = await Token.findOne({ where: { token } })
-        const isValidToken=jwt.verify(token,process.env.JWT)
-        if (isAuthUser&&!!isValidToken) {
+        const isValidToken = jwt.verify(token, process.env.JWT)
+        if (isAuthUser && !!isValidToken) {
             res.send(user)
         } else {
             throw new Error({ error: 'unauthorized' })
@@ -55,6 +55,34 @@ router.get('/authUser', async (req, res) => {
         console.log(errorStyle(e));
     }
 
+})
+router.get('/iValidUserName', async (res, res) => {
+    try {
+        const { userName } = req.body;
+        const userName = await User.findOne({ where: { userName } })
+        if (userName) {
+            throw new Error({ error: 'This user name already exists!' })
+        } else {
+            res.send()
+        }
+    } catch (e) {
+        res.status(400).send(e)
+        console.log(errorStyle(e));
+    }
+})
+router.get('/iValidEmail', async (res, res) => {
+    try {
+        const { email } = req.body;
+        const email = await User.findOne({ where: { email } })
+        if (email) {
+            throw new Error({ error: 'This email address already exists!' })
+        } else {
+            res.send()
+        }
+    } catch (e) {
+        res.status(400).send(e)
+        console.log(errorStyle(e));
+    }
 })
 
 router.get('/', async (req, res) => {
